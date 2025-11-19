0fcade5c93f641e01d055702ebef2ee4a0f1e39d
Daria <darijazeml@inbox.ru>
2025-11-19 22:24:28 +0300

    Clang format and corrected number structure in lexer

--- source/main.cpp
+++ source/main.cpp
@@ -1,13 +1,13 @@
-
 #include <iostream>
 
 #include <FlexLexer.h>
 
-int yyFlexLexer::yywrap() { return 1; }
+int main()
+{
+    FlexLexer* lexer = new yyFlexLexer;
+    while (lexer->yylex() != 0)
+    {
+    }
 
-int main() {
-  FlexLexer *lexer = new yyFlexLexer;
-  while (lexer->yylex() != 0) {}
-
-  delete lexer;
+    delete lexer;
 }
